4d198f3e803606d08aa30b9a01dd34f9eacb9fb5
samarth06feb-byte <samarth06feb@gmail.com>
2026-01-17 18:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ if ticker_symbol:
             st.warning("Category: Fast Grower")
         else:
             st.info("Category: Monitor for Turnaround or Cyclicality")
+            
     # TAB 5: VOLATILITY & RISK ANALYSIS
     import streamlit as st
 import yfinance as yf
